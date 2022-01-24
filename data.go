45c6e8c0cab97e77ab06dff8a4933d8860ad1d77
Brian Mayer <bleemayer@gmail.com>
2022-01-24 17:06:36 -0300

    Added terms of service

--- data.go
+++ data.go
@@ -164,7 +164,96 @@ link: <a href="/login">login page</a>.</p>
 		Title:       "Create your user",
 		List:        "tasks",
 		Summary:     "The description of this task has a link to the registration page.",
-		Description: `<p>Here is the link: <a href="/register">registration page</a>. Welcome!</p>`,
+		Description: `<p>Here is the link: <a href="/register">registration page</a>. Welcome!</p>
+<h3>Terms and conditions</h3>
+<p>
+These terms and conditions (“Agreement”) set forth the general terms and conditions of your use of the
+tasker.saucecode.bar website (“Website” or “Service”) and any of its related products and
+services (collectively, “Services”). This Agreement is legally binding between you (“User”, “you” or “your”)
+and this Website operator (“Operator”, “we”, “us” or “our”). If you are entering into this agreement on behalf
+of a business or other legal entity, you represent that you have the authority to bind such entity to this agreement
+, in which case the terms “User”, “you” or “your” shall refer to such entity. If you do not have such authority, or
+if you do not agree with the terms of this agreement, you must not accept this agreement and may not access and use
+the Website and Services. By accessing and using the Website and Services, you acknowledge that you have read,
+understood, and agree to be bound by the terms of this Agreement. You acknowledge that this Agreement is a contract
+between you and the Operator, even though it is electronic and is not physically signed by you, and it governs your
+use of the Website and Services. This terms and conditions policy was created with the help of the terms and
+conditions generator at https://www.websitepolicies.com/terms-and-conditions-generator
+</p>
+<h3>Accounts and membership</h3>
+<p>
+If you create an account on the Website, you are responsible for maintaining the security of your account and you
+are fully responsible for all activities that occur under the account and any other actions taken in connection with
+it. We may, but have no obligation to, monitor and review new accounts before you may sign in and start using the
+Services. Providing false contact information of any kind may result in the termination of your account. You must
+immediately notify us of any unauthorized uses of your account or any other breaches of security. We will not be
+liable for any acts or omissions by you, including any damages of any kind incurred as a result of such acts
+or omissions.
+</p>
+<h3>User content</h3>
+<p>
+We do not own any data, information or material (collectively, “Content”) that you submit on the Website in the
+course of using the Service. You shall have sole responsibility for the accuracy, quality, integrity, legality,
+reliability, appropriateness, and intellectual property ownership or right to use of all submitted Content. We
+may, but have no obligation to, monitor and review the Content on the Website submitted or created using our
+Services by you. You grant us permission to access, copy, distribute, store, transmit, reformat, display and
+perform the Content of your user account solely as required for the purpose of providing the Services to you.
+Unless specifically permitted by you, your use of the Website and Services does not grant us the license to use,
+reproduce, adapt, modify, publish or distribute the Content created by you or stored in your user account for
+commercial, marketing or any similar purpose.
+</p>
+<h3>Adult content</h3>
+<p>
+Please be aware that there may be certain adult or mature content available on the Website. Where there is mature
+or adult content, individuals who are less than 18 years of age or are not permitted to access such content under
+the laws of any applicable jurisdiction may not access such content. If we learn that anyone under the age of 18
+seeks to conduct a transaction through the Services, we will require verified parental consent, in accordance with
+the Children’s Online Privacy Protection Act of 1998 (“COPPA”). Certain areas of the Website and Services may not
+be available to children under 18 under any circumstances.
+</p>
+<h3>Backups</h3>
+<p>
+We are not responsible for the Content residing on the Website. In no event shall we be held liable for any loss
+of any Content. It is your sole responsibility to maintain appropriate backup of your Content. Notwithstanding the
+foregoing, on some occasions and in certain circumstances, with absolutely no obligation, we may be able to restore
+some or all of your data that has been deleted as of a certain date and time when we may have backed up data for our
+own purposes. We make no guarantee that the data you need will be available.
+</p>
+<h3>Links to other resources</h3>
+<p>
+Although the Website and Services may link to other resources (such as websites, mobile applications, etc.), we are
+not, directly or indirectly, implying any approval, association, sponsorship, endorsement, or affiliation with any
+linked resource, unless specifically stated herein. We are not responsible for examining or evaluating, and we do
+not warrant the offerings of, any businesses or individuals or the content of their resources. We do not assume any
+responsibility or liability for the actions, products, services, and content of any other third parties. You should
+carefully review the legal statements and other conditions of use of any resource which you access through a link
+on the Website. Your linking to any other off-site resources is at your own risk.
+</p>
+<h3>Changes and amendments</h3>
+<p>
+We reserve the right to modify this Agreement or its terms related to the Website and Services at any time at our
+discretion. When we do, we will post a notification on the main page of the Website. We may also provide notice to
+you in other ways at our discretion, such as through the contact information you have provided.
+</p>
+<p>
+An updated version of this Agreement will be effective immediately upon the posting of the revised Agreement
+unless otherwise specified. Your continued use of the Website and Services after the effective date of the revised
+Agreement (or such other act specified at that time) will constitute your consent to those changes.
+</p>
+<h3>Acceptance of these terms</h3>
+<p>
+You acknowledge that you have read this Agreement and agree to all its terms and conditions. By accessing and using
+the Website and Services you agree to be bound by this Agreement. If you do not agree to abide by the terms of this
+Agreement, you are not authorized to access or use the Website and Services.
+</p>
+<h3>Contacting us</h3>
+<p>
+If you have any questions, concerns, or complaints regarding this Agreement, we encourage you to contact us using
+the details below:
+</p>
+<a href="mailto://bleemayer@gmail.com">bleemayer@gmail.com</a>
+<p>
+This document was last updated on January 24, 2022</p>`,
 		Status:      "Active",
 		Creator:     "blmayer",
 		Date:        time.Date(2021, 8, 16, 22, 10, 54, 0, time.UTC),
